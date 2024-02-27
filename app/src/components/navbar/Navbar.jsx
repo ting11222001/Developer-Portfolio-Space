29672ddb79a44c1514ee0070c5187b69a4ab5b12
tiffany <ting11222001@gmail.com>
2024-02-28 00:36:46 +1030

    Fix layout.

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
       {/* Sidebar */}
       <Sidebar />
       <div className="wrapper">
-        <motion.span 
-        initial={{opacity: 0, scale: 0.5}}
-        animate={{opacity: 1, scale: 1}}
-        transition={{duration: 0.5}}
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
         >
           Codify.
-          </motion.span>
+        </motion.span>
         <div className="social">
           <a href="#"><img src="/facebook.png" alt="" /></a>
           <a href="#"><img src="/instagram.png" alt="" /></a>
