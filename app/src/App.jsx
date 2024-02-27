8d0c22f61e05b380952cab31e2a5496f0139844f
tiffany <ting11222001@gmail.com>
2024-02-27 12:01:43 +1030

    Create Parallax section.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 function App() {
 
@@ -11,13 +12,25 @@ function App() {
         <Navbar />
         <Hero />
       </section>
-      <section id="Services">Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section>Services</section>
-      <section id="Portfolio">Parallax</section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section id="Contact">Contact</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <section>
+        Portfolio1
+      </section>
+      <section>
+        Portfolio2
+      </section>
+      <section>
+        Portfolio3
+      </section>
+      <section id="Contact">
+        Contact
+      </section>
     </div>
 
 
