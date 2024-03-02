8af99815c1e43734048b11ceb4df87c89b285714
tiffany <ting11222001@gmail.com>
2024-03-02 12:19:54 +1030

    FIx navbar and hamburger menu.

--- app/src/components/navbar/sidebar/Sidebar.jsx
+++ app/src/components/navbar/sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ const variants = {
     }
   },
   closed: {
-    clipPath: "circle(30px at 50px 50px)",
+    clipPath: "circle(30px at 350px 50px)",
     transition: {
       delay: 0.5,
       type: "spring",
